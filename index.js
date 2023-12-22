443e35767b3fb26c8da50f5bccbd1a02172dfec4
Vihan Jayasinghe <100029985+IT21314742@users.noreply.github.com>
2023-12-22 10:11:06 +0530

    Update index.js
    Develop const files

---
+++ index.js
@@ -0,0 +1,19 @@
+const testimonials = [
+    {
+        name: "Cherise G",
+        photoUrl: "284.jpg",
+        text: "This is simply unbelievable! I would be lost without APPPLE. the very best. Not able to tell you how happy i am with apple.",
+    },
+
+    {
+        name: "Dihan K",
+        photoUrl: "21F1Silverstone1214.jpg",
+        text: "This is simply unbelievable! I would be lost without APPPLE. the very best. Not able to tell you how happy i am with apple.",
+    },
+
+    {
+        name: "Vihan M",
+        photoUrl: "21F1Silverstone1214.jpg",
+        text: "This is simply unbelievable! I would be lost without APPPLE. the very best. Not able to tell you how happy i am with apple.",
+    },
+];
